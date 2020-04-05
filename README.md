189c78d088a5aa0448e419428f45c8832c3041bf
Ken Jenney <kjenney@gmail.com>
2020-04-05 16:07:31 -0400

    First clue statement

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 
 A Python script to calculate Brain Teasers
 
+Example:
+
 ```
 682 1 digit is right and in it's place
 614 1 digit is right in the wrong place
 206 two digits are right both in the wrong place
 738 all digits are wrong
 380 1 digit is right in wrong place
+```
+
+How to Run It with the Script:
+
+```
+python tease.py --first_clue_digits 681 \
+               --first_clue_context_digits 1 \
+               --first_clue_context_boolean right \
+               --first_clue_context_place correct
 ```
--- tease.py
+++ tease.py
@@ -1,3 +1,19 @@
 from pprint import pprint
+import argparse
 
 # Script to solve Brain Teasers
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--first_clue_digits", help="first clue digits", required=True)
+parser.add_argument("--first_clue_context_digits", help="the number of digits to match", required=True)
+parser.add_argument("--first_clue_context_boolean", help="is/are digits right or wrong?", choices=['right', 'wrong'], required=True)
+parser.add_argument("--first_clue_context_place", help="is/are the digit(s) in the correct or incorrect place?", choices=['correct', 'incorrect'], required=True)
+args = parser.parse_args()
+
+number_of_digits = len(args.first_clue_digits)
+print("Here is the teaser: Find a " + str(number_of_digits) + " digit number that matches the following requirements:")
+if int(args.first_clue_context_digits) > 1:
+    context_digits_language = args.first_clue_context_digits + " digits that are "
+else:
+    context_digits_language = args.first_clue_context_digits + " digit that is "
+print('\tThe number ' + args.first_clue_digits + ' has ' + context_digits_language + args.first_clue_context_boolean + ' and in the ' + args.first_clue_context_place + ' place')
