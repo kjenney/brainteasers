532a5818ce51d972975cca2d696bcfc7921decd4
Ken Jenney <kjenney@gmail.com>
2020-04-05 23:42:31 -0400

    Add a class

--- tease.py
+++ tease.py
@@ -1,7 +1,37 @@
 from pprint import pprint
 import argparse
+from random import randint
 
 # Script to solve Brain Teasers
+
+class MatchMe:
+    """Match a number against requirements"""
+    def __init__(self, number, args):
+        self.number = number
+        self.args = args
+        self.solve()
+
+    def how_many_digits_to_match(self):
+        return int(self.args.first_clue_context_digits)
+
+    def digits_boolean(self):
+        """Is the digit there or not?"""
+        if self.args.first_clue_context_boolean == 'right':
+            return True
+        else:
+            return False
+
+    def match_number_of_digits(self):
+        if self.digits_boolean:
+            for i in range(0, self.how_many_digits_to_match()):
+                print(i)
+
+    def solve(self):
+        self.match_number_of_digits()
+        #if str(self.args.first_clue_digits)[0] in str(self.number):
+        #    return True
+        #else:
+        #    return False
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--first_clue_digits", help="first clue digits", required=True)
@@ -17,3 +47,19 @@ if int(args.first_clue_context_digits) > 1:
 else:
     context_digits_language = args.first_clue_context_digits + " digit that is "
 print('\tThe number ' + args.first_clue_digits + ' has ' + context_digits_language + args.first_clue_context_boolean + ' and in the ' + args.first_clue_context_place + ' place')
+
+# Generate random numbers of number_of_digits in length until 1 matches the requirement
+min = 0
+max = int('9' * number_of_digits)
+while True:
+    matched = False
+    digits = [str(randint(min, max)) for i in range(5)]
+    for digit in digits:
+        number = (len(str(max))-len(digit))*'0'+digit
+        MatchMe(number, args)
+        break
+        #if matched:
+        #    print(number)
+        #    break
+    #if matched:
+    break
